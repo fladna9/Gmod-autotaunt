19b4f473b192ab6a9168381520ddff0b76913b6f
Maxence <contact@fladnag.net>
2016-05-10 21:25:31 +0200

    Added settings part and updated Readme
    Added settings part and updated Readme

--- README.md
+++ README.md
@@ -1,18 +1,43 @@
-# Gmod-autotaunt project
-A Garry's Mod lua script that makes props auto taunt every 1 minute
+# Garry's Mod Auto Taunt Addon project
+A Garry's Mod lua script that makes props auto taunt
 
 As not any of these plugins where available on the Internet, decided to create it myself.
-This is my first lua project, be nice ;)
+This is my first lua project, please be nice ;)
+
+If you want to contribute/fork, don't hesitate :D
+
 
 ## How does it work
-On player spawn, if player is a prop, it creates a timer that will sound the "This is Sparta" taunt every minute.
+On player spawn, if player is a prop, it creates a timer that will sound a random taunt.
+
 On player death, if player is a prop, it removes the timer.
-It also prints a line in the console for each prop auto taunting.
+
+It also prints a line in the server console for each prop auto taunting.
+
 
 ## How to use
-Create directories and copy autotaunt.lua in <dedicated server files>/garrysmod/addons/autotaunt/lua/autorun/autotaunt.lua
-Restart your server and you're ok to go.
+Make sure your server has the following addons : 
+* "Prop Hunt New Taunts" : http://steamcommunity.com/sharedfiles/filedetails/?id=272425836
+* "Prop Hunt Tauntpack Loader" : http://steamcommunity.com/sharedfiles/filedetails/?id=272852615
+* A Prop Hunt addon (tested on "Wolvin's: Enhanced Prop Hunt" : http://steamcommunity.com/sharedfiles/filedetails/?id=272852615 )
+
+Create missing directories and copy *autotaunt.lua* in 
+```
+<dedicated server files>/garrysmod/addons/autotaunt/lua/autorun/autotaunt.lua
+```
+Restart your server and you're good to go.
+
+## How to configure
+This lua file has a settings part for you to easily edit the behaviour of the addon, without having to dig into the code.
+```lua
+-- Change here the time you want between 2 taunts
+timeBetweenTwoTaunts = 60
+-- Change here how many times you want this auto taunt be be executed in a round
+howManyTimes = 5
+-- The number of taunts available. Can find with ph_tauntlist in console
+maxTaunts = 75
+```
 
 ## Licence
-This project is licensed under GNU/GPL v3
+This project is licensed under GNU/GPL v3. 
 You can find a version of the license at http://www.gnu.org/licenses/gpl-3.0.en.html
--- autotaunt.lua
+++ autotaunt.lua
@@ -1,21 +1,51 @@
+------------------------------------------------------------------------
+-- Garry's Mod Auto Taunt Addon                                       --
+-- https://github.com/fladna9/Gmod-autotaunt                          --
+--                                                                    --
+-- Francis Team, All Rights Reserved (2016)                           --
+--                                                                    --
+-- This addon's license is GNU/GPL v3                                 --
+-- You can find it here : http://www.gnu.org/licenses/gpl-3.0.en.html --
+------------------------------------------------------------------------
+
+
+-- IF YOU'RE NOT FAMILIAR WITH LUA, EDIT JUST BELOW THE SETTINGS FOR THE ADDON --
+
+-- Change here the time you want between 2 taunts
+timeBetweenTwoTaunts = 60
+-- Change here how many times you want this auto taunt be be executed in a round
+howManyTimes = 5
+-- The number of taunts available. Can find with ph_tauntlist in console
+maxTaunts = 75
+
+-- END OF SETTINGS --
+
+
+autoTauntTimerName = "Francis_Auto_Taunt_Timer"
+
+-- Main autoTaunt function
 local function autoTaunt( ply )
+		-- Auto_Taunt only if prop
         if(ply:Team() == TEAM_PROPS) then
-                timer.Create("Auto_Taunt", 60, 5, function ()
+				-- Creation of the timer
+                timer.Create(autoTauntTimerName, timeBetweenTwoTaunts, howManyTimes, function ()
+						-- Initializing seed for random number
 						math.randomseed(os.time())
-						local tauntNumber = math.random(75)
-                        ply:ConCommand( "ph_taunt team_props " + tauntNumber )
+						-- Generate a random number between 1 and maxTaunts 
+						local tauntNumber = math.random(maxTaunts)
+                        ply:ConCommand( "ph_taunt team_props " ..tauntNumber )
                         print( ply:Nick().. " has just auto taunted to help hunters !" )
                 end)
         end
 end
 
+-- Used to remove the timer on player death
 local function removeAutoTaunt( ply )
         if(ply:Team() == TEAM_PROPS) then
-                timer.Remove("Auto_Taunt")
+                timer.Remove(autoTauntTimerName)
         end
 end
 
-
+-- Add functions to hooks
 hook.Add("PlayerSpawn", "Player_Spawned_AutoTaunt", autoTaunt )
 hook.Add("PlayerDeath", "Player_Died_AutoTaunt", removeAutoTaunt )
-hook.Add("PlayerSay", "playersaystuck", 
