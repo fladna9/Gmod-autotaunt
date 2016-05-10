fadc4757ce01d943fa6f65c83d98e12f9eb80d40
Maxence <contact@fladnag.net>
2016-05-10 20:26:31 +0200

    Changed to random taunt
    Changed from a static to a random prop :)

--- autotaunt.lua
+++ autotaunt.lua
@@ -1,7 +1,9 @@
 local function autoTaunt( ply )
         if(ply:Team() == TEAM_PROPS) then
                 timer.Create("Auto_Taunt", 60, 5, function ()
-                        ply:ConCommand( "ph_taunt team_props 20" )
+						math.randomseed(os.time())
+						local tauntNumber = math.random(75)
+                        ply:ConCommand( "ph_taunt team_props " + tauntNumber )
                         print( ply:Nick().. " has just auto taunted to help hunters !" )
                 end)
         end
@@ -13,5 +15,7 @@ local function removeAutoTaunt( ply )
         end
 end
 
+
 hook.Add("PlayerSpawn", "Player_Spawned_AutoTaunt", autoTaunt )
 hook.Add("PlayerDeath", "Player_Died_AutoTaunt", removeAutoTaunt )
+hook.Add("PlayerSay", "playersaystuck", 
